80a1a2161d350446759636bf6fc89415c4f7cba8
veerbal1 <veerbal1singh1@gmail.com>
2022-01-10 21:56:18 +0530

    Enabled object move left and right

---
+++ Matter.js/src/index.js
@@ -0,0 +1,104 @@
+import './style.scss';
+import Matter from 'matter-js';
+import image from './asset/webp.webp';
+
+var Engine = Matter.Engine;
+var Render = Matter.Render;
+var Runner = Matter.Runner;
+var Composite = Matter.Composite;
+var Bodies = Matter.Bodies;
+
+// elements
+let leftButton;
+let rightButton;
+
+window.onload = () => {
+  leftButton = document.getElementById('left');
+  rightButton = document.getElementById('right');
+  // Create an engine
+  var engine = Engine.create({});
+
+  // Create an renderer
+  var render = Render.create({
+    element: document.body,
+    engine: engine,
+    options: {
+      width: window.innerWidth,
+      height: window.innerHeight,
+      wireframes: false,
+      background: 'teal',
+      backgroundScale: 1.2,
+      // Cover the entire screen
+    },
+  });
+
+  //   Create two boxes and a ground
+  var boxA = Bodies.rectangle(200, 200, 80, 80);
+  var ground = Bodies.rectangle(
+    window.innerWidth / 2,
+    window.innerHeight - 40,
+    window.innerWidth,
+    40,
+    {
+      isStatic: true,
+    }
+  );
+
+  //   Composite all of the bodies to the world
+  Composite.add(engine.world, [boxA, ground]);
+
+  //   Run the renderer
+  Render.run(render);
+
+  //   Create runner
+  var runner = Runner.create();
+
+  window.onkeydown = function (e) {
+    if (e.keyCode == 37) {
+      Matter.Body.setPosition(boxA, {
+        x: boxA.position.x - 10,
+        y: boxA.position.y,
+      });
+    } else if (e.keyCode == 39) {
+      Matter.Body.setPosition(boxA, {
+        x: boxA.position.x + 10,
+        y: boxA.position.y,
+      });
+    }
+  };
+  eventListeners(boxA);
+  //   Run the engine
+  Runner.run(runner, engine);
+};
+
+const eventListeners = (boxA) => {
+  // On key pressed
+  onTouch(leftButton, () => {
+    Matter.Body.setPosition(boxA, {
+      x: boxA.position.x - 10,
+      y: boxA.position.y,
+    });
+  });
+
+  onTouch(rightButton, () => {
+    Matter.Body.setPosition(boxA, {
+      x: boxA.position.x + 10,
+      y: boxA.position.y,
+    });
+  });
+};
+
+const onTouch = (element, callback) => {
+  let interval;
+  // Keep running on touch start and stop on touch end
+  element.addEventListener('touchstart', () => {
+    interval = setInterval(() => {
+      callback();
+    }, 16);
+  });
+
+  // on touch end
+  element.addEventListener('touchend', () => {
+    clearInterval(interval);
+  });
+};
