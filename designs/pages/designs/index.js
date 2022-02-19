10d9e219b606141ead37c94a787c5f22c59e1951
veerbal1 <veerbal1singh1@gmail.com>
2022-02-19 21:09:24 +0530

    temp.

---
+++ designs/pages/designs/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import classes from '../../styles/Designs.module.scss';
+const Designs = () => {
+  return <div className={classes.main}>Designs</div>;
+};
+
+export default Designs;
---
+++ my-app/src/renderer.js
@@ -0,0 +1,3 @@
+window.onload = () => {
+  document.body.style.backgroundColor = '#ff0';
+};
