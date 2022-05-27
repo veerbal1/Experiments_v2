67ac1aec12fd78eca93c6e59d28948bab7dfd7ad
veerbal1 <veerbal1singh1@gmail.com>
2022-05-27 14:46:29 +0530

    creating simple wave

--- curve clip/index.js
+++ curve clip/index.js
@@ -10,18 +10,26 @@ window.onload = () => {
   const bottomLeft = '0 100%';
   const bottomRight = '100% 100%';
   const topRight = '100% 0';
-  const others = '85% 12%, 60% 16%, 41% 16%, 30% 18%, 16% 13%';
+//   let others = '85% 12%, 60% 16%, 41% 16%, 30% 18%, 16% 13%';
+    let others = '0 0';
 
   let x = 100;
   const constantVal = Math.PI / x;
   while (x !== -1) {
     const radian = Number((constantVal * x).toFixed(5));
     const y = Math.sin(radian);
-    console.log(x, radian, y);
+
+    console.log(x, radian, Number((100 - y * 100).toFixed(6)));
+    others += `,${x}% ${Number((y * 100).toFixed(6))}% `;
+
     x--;
   }
+  // console.log(others);
   element.style.clipPath = `polygon(${topLeft}, ${bottomLeft}, ${bottomRight}, ${topRight}, ${others})`;
-  //   element.style.clipPath = `polygon(0 0, 0 100%, 100% 100%, 100% 0, 85% 12%, 60% 16%, 41% 16%, 30% 18%, 16% 13%)`;
+  console.log(
+    `polygon(${topLeft}, ${bottomLeft}, ${bottomRight}, ${topRight} ${others})`
+  );
+  // element.style.clipPath = `polygon(0 0, 0 100%, 100% 100%, 100% 0, 85% 12%, 60% 16%, 41% 16%, 30% 18%, 16% 13%)`;
 };
 
 const rdP = () => {
